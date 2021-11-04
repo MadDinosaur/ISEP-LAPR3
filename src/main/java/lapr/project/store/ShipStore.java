45af0ab286f82886e6ef13d583353472de4a5a4d
Carlos1200991 <1200991@isep.ipp.pt>
2021-11-04 16:19:15 +0000

    LAP22G21-1 #comment changed the store class in order to accomodate a faster and more usefull way to get a ship's data

--- src/main/java/lapr/project/store/ShipStore.java
+++ src/main/java/lapr/project/store/ShipStore.java
@@ -10,7 +10,20 @@ import java.util.*;
 
 public class ShipStore {
 
-    private BST<Ship> shipTree = new BST<>();
+    /**
+     * A tree sorted by the ship's mssi values
+     */
+    private Tree_Ship mmsiTree = new Tree_Ship();
+
+    /**
+     * A tree sorted by the ship's imo value
+     */
+    private Tree_Ship imoTree = new Tree_Ship();
+
+    /**
+     * A tree sorted by the ship's Call Sign value
+     */
+    private Tree_Ship callSignTree = new Tree_Ship();
 
     /**
      * transforms a list of dtos into a list of Ships
@@ -28,25 +41,10 @@ public class ShipStore {
      */
     public boolean addShip(Ship ship){
         if (validateShip(ship)){
-            if (!existShip(ship)){
-                shipTree.insert(ship);
-                return true;
-            }
-        }
-        return false;
-    }
-
-    /**
-     * returns true if a the ship is already in the tree
-     * @param ship the ship to be analyzed
-     * @return returns true if a the ship is already in the tree
-     */
-    private boolean existShip(Ship ship){
-        for (Ship otherShip : shipTree.inOrder()) {
-            if (otherShip.getMmsi().equals(ship.getMmsi()) || otherShip.getImo() == ship.getImo() || otherShip.getCallSign().equals(ship.getCallSign())) {
-                otherShip.getPositioningDataList().addPositioningDataList(ship.getPositioningDataList().getPositioningDataList());
-                return true;
-            }
+            imoTree.insertIMO(ship);
+            mmsiTree.insertMMSI(ship);
+            callSignTree.insertCallSign(ship);
+            return true;
         }
         return false;
     }
@@ -61,23 +59,18 @@ public class ShipStore {
     }
 
     /**
-     * searches for ship by it's mmsi code
-     * @param code a mmsi code
-     * @return a ship with the given mmsi code or null
+     * returns a ship with a similar mmsi
+     * @param code an mmsi code of a possible ship
+     * @return returns a ship if it exists
      */
     public Ship getShipByMMSI(String code){
-        for (Ship otherShip : shipTree.inOrder()){
-            if (otherShip.getMmsi().equals(code)){
-                return otherShip;
-            }
-        }
-        return null;
+        return mmsiTree.findMMSI(code);
     }
 
     /**
-     * searches for ship by it's imo code
-     * @param code a imo code
-     * @return a ship with the given imo code or null
+     * returns a ship with a similar imo
+     * @param code an imo code of a possible ship
+     * @return returns a ship if it exists
      */
     public Ship getShipByIMO(String code){
         int codeImo;
@@ -86,141 +79,15 @@ public class ShipStore {
         } catch (NumberFormatException e) {
             codeImo = 0;
         }
-        for (Ship otherShip : shipTree.inOrder()){
-            if (otherShip.getImo() == codeImo){
-                return otherShip;
-            }
-        }
-        return null;
+        return imoTree.findIMO(codeImo);
     }
 
     /**
-     * searches for ship by it's Call sign code
-     * @param code a Call sign code
-     * @return a ship with the given Call sign code or null
+     * returns a ship with a similar Call Sign
+     * @param code an Call Sign code of a possible ship
+     * @return returns a ship if it exists
      */
     public Ship getShipByCallSign(String code){
-        for (Ship otherShip : shipTree.inOrder()){
-            if (otherShip.getCallSign().equals(code)){
-                return otherShip;
-            }
-        }
-        return null;
+        return callSignTree.findCallSign(code);
     }
-
-    //    /**
-//     * A bst tree sorted by the ship's mmsi
-//     */
-//    private TreeSet<Ship> mmsiSortedTree;
-//
-//    /**
-//     * A bst tree sorted by the ship's imo
-//     */
-//    private TreeSet<Ship> imoSortedTree;
-//
-//    /**
-//     * A bst tree sorted by the ship's Call Sign
-//     */
-//    private TreeSet<Ship> callSignSortedTree;
-//
-//    /**
-//     * starts the different trees with different comparators.
-//     */
-//    public ShipStore(){
-//        mmsiSortedTree = new TreeSet<>(new Comparator<Ship>() {
-//            @Override
-//            public int compare(Ship o1, Ship o2) {
-//                return o1.getMmsi().compareTo(o2.getMmsi());
-//            }
-//        });
-//
-//        imoSortedTree = new TreeSet<>(new Comparator<Ship>() {
-//            @Override
-//            public int compare(Ship o1, Ship o2) {
-//                return Integer.compare(o1.getImo(), o2.getImo());
-//            }
-//        });
-//
-//        callSignSortedTree = new TreeSet<>(new Comparator<Ship>() {
-//            @Override
-//            public int compare(Ship o1, Ship o2) {
-//                return o1.getCallSign().compareTo(o2.getCallSign());
-//            }
-//        });
-//    }
-//
-//    /**
-//     * transforms a list of dtos into a list of Ships
-//     * @param shipData a map with all the ship information and their positioning data
-//     * @return a ship list
-//     */
-//    public List<Ship> createShip(Map<ShipDTO, List<PositioningDataDTO>> shipData){
-//        return ShipMapper.toModel(shipData);
-//    }
-//
-//    /**
-//     * ãdd a new ship to the tree, if the ship already exists simply adds the positioning data
-//     * @param ship the ship to be added
-//     * @return true if the ship or the ship data is added successfully
-//     */
-//    public boolean addShip(Ship ship){
-//        if (validateShip(ship)){
-//            if (!existShip(ship)){
-//                return callSignSortedTree.add(ship) &&  imoSortedTree.add(ship) && mmsiSortedTree.add(ship);
-//            } else {
-//                for (Ship otherShip : mmsiSortedTree.descendingSet()) {
-//                    if (otherShip.getMmsi().equals(ship.getMmsi()) || otherShip.getImo() == ship.getImo() || otherShip.getCallSign().equals(ship.getCallSign()))
-//                        otherShip.getPositioningDataList().addPositioningDataList(ship.getPositioningDataList().getPositioningDataList());
-//                }
-//            }
-//        }
-//        return false;
-//    }
-//
-//    /**
-//     * returns true if a the ship is already in the tree
-//     * @param ship the ship to be analyzed
-//     * @return returns true if a the ship is already in the tree
-//     */
-//    private boolean existShip(Ship ship){
-//        for (Ship otherShip : mmsiSortedTree.descendingSet()) {
-//            if (otherShip.getMmsi().equals(ship.getMmsi()) || otherShip.getImo() == ship.getImo() || otherShip.getCallSign().equals(ship.getCallSign()))
-//                   return true;
-//           }
-//        return false;
-//    }
-//
-//    /**
-//     * validates the ship to be added
-//     * @param ship a ship
-//     * @return return true if the ship is not null
-//     */
-//    private boolean validateShip(Ship ship){
-//        return ship != null;
-//    }
-//
-//
-//    private Ship getShipByMmsi(String mmsi){
-//        return  null;
-//    }
-//
-//    /**
-//     * returns a ship by one of it's codes
-//     * @param code one of the ships main codes
-//     * @return returns a ship with the entered code or none at all
-//     */
-//    public Ship getShipByCode(String code){
-//        int codeImo;
-//        try{
-//            codeImo = Integer.parseInt(code);
-//        } catch (NumberFormatException e) {
-//            codeImo = 0;
-//        }
-//        for (Ship otherShip : shipTree.inOrder()){
-//            if (otherShip.getImo() == codeImo || otherShip.getMmsi().equals(code) || otherShip.getCallSign().equals(code)){
-//                return otherShip;
-//            }
-//        }
-//        return null;
-//    }
 }
---
+++ src/test/java/lapr/project/model/CoordinateTest.java
@@ -0,0 +1,62 @@
+package lapr.project.model;
+
+import lapr.project.exception.IllegalCoordinateException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CoordinateTest {
+
+
+
+    @Test
+    void CreateCoordinatesTest() {
+        Coordinate c1 = new Coordinate(180, 80);
+        Coordinate c2 = new Coordinate(-180, 91);
+        Coordinate c3 = new Coordinate(181, -80);
+        assertNotNull(c1);
+        assertNotNull(c2);
+        assertNotNull(c3);
+    }
+
+    @Test
+    public void testIllegalLongitudeValues() {
+        Exception exception1 = assertThrows(IllegalArgumentException.class, () -> {
+            Coordinate a = new Coordinate((float) -180.1, 50);
+        });
+        Exception exception2 = assertThrows(IllegalArgumentException.class, () -> {
+            Coordinate a = new Coordinate((float) 180.1, 50);
+        });
+
+        String expectedMessage1 = "The longitude value \"-180.1\" is not within the expected boundaries";
+        String actualMessage1 = exception1.getMessage();
+
+        assertTrue(actualMessage1.contains(expectedMessage1));
+
+        String expectedMessage2 = "The longitude value \"180.1\" is not within the expected boundaries";
+        String actualMessage2 = exception2.getMessage();
+
+        System.out.println(actualMessage2);
+        assertTrue(actualMessage2.contains(expectedMessage2));
+    }
+
+    @Test
+    public void testIllegalLatitudeValues() {
+        Exception exception1 = assertThrows(IllegalArgumentException.class, () -> {
+            Coordinate a = new Coordinate( 90, (float) 90.1);
+        });
+        Exception exception2 = assertThrows(IllegalArgumentException.class, () -> {
+            Coordinate a = new Coordinate( 90, (float) -90.1);
+        });
+
+        String expectedMessage1 = "The latitude value \"90.1\" is not within the expected boundaries";
+        String actualMessage1 = exception1.getMessage();
+
+        assertTrue(actualMessage1.contains(expectedMessage1));
+
+        String expectedMessage2 = "The latitude value \"-90.1\" is not within the expected boundaries";
+        String actualMessage2 = exception2.getMessage();
+
+        assertTrue(actualMessage2.contains(expectedMessage2));
+    }
+}
