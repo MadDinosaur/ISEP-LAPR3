6d66c34e18e3431e7c6499d7c6b3a31ed5128972
Carlos1200991 <1200991@isep.ipp.pt>
2021-11-05 14:20:33 +0000

    LAP22G21-2 #comment cehnged the return method so that it also returns the ships positioning data

--- src/main/java/lapr/project/controller/SearchForShipController.java
+++ src/main/java/lapr/project/controller/SearchForShipController.java
@@ -1,10 +1,16 @@
 package lapr.project.controller;
 
+import lapr.project.mappers.PositioningDataMapper;
 import lapr.project.mappers.ShipMapper;
+import lapr.project.mappers.dto.PositioningDataDTO;
 import lapr.project.mappers.dto.ShipDTO;
 import lapr.project.model.Ship;
 import lapr.project.store.ShipStore;
 import lapr.project.store.MainStorage;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class SearchForShipController {
 
@@ -29,27 +35,33 @@ public class SearchForShipController {
         this.shipStore = mainStorage.getShipStore();
     }
 
-    public ShipDTO getShipByMMSI(String mmsi){
+    public Map<ShipDTO, List<PositioningDataDTO>> getShipByMMSI(String mmsi){
         Ship ship = shipStore.getShipByMMSI(mmsi);
-        if (ship != null)
-            return ShipMapper.toDTO(ship);
-        else
+        Map<ShipDTO, List<PositioningDataDTO>> map = new HashMap<>();
+        if (ship != null) {
+            map.put(ShipMapper.toDTO(ship), PositioningDataMapper.toDTO(ship.getPositioningDataList().getPositioningDataList()));
+            return map;
+        } else
             return null;
     }
 
-    public ShipDTO getShipByIMO(String imo){
+    public Map<ShipDTO, List<PositioningDataDTO>> getShipByIMO(String imo){
         Ship ship = shipStore.getShipByIMO(imo);
-        if (ship != null)
-            return ShipMapper.toDTO(ship);
-        else
+        Map<ShipDTO, List<PositioningDataDTO>> map = new HashMap<>();
+        if (ship != null) {
+            map.put(ShipMapper.toDTO(ship), PositioningDataMapper.toDTO(ship.getPositioningDataList().getPositioningDataList()));
+            return map;
+        } else
             return null;
     }
 
-    public ShipDTO getShipByCallSign(String callSign){
+    public Map<ShipDTO, List<PositioningDataDTO>> getShipByCallSign(String callSign){
         Ship ship = shipStore.getShipByCallSign(callSign);
-        if (ship != null)
-            return ShipMapper.toDTO(ship);
-        else
+        Map<ShipDTO, List<PositioningDataDTO>> map = new HashMap<>();
+        if (ship != null) {
+            map.put(ShipMapper.toDTO(ship), PositioningDataMapper.toDTO(ship.getPositioningDataList().getPositioningDataList()));
+            return map;
+        } else
             return null;
     }
 }
