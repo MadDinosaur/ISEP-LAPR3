00d8087d56718d4044f5c13ed7f06927a5bdf763
Miguel <1201045@isep.ipp.pt>
2021-11-07 16:35:05 +0000

    LAP22G21-5 #comment  Implementation added (50% done)

---
+++ src/main/java/lapr/project/utils/ShipSorter.java
@@ -0,0 +1,41 @@
+package lapr.project.utils;
+
+import lapr.project.model.Ship;
+
+import java.util.Comparator;
+
+public class ShipSorter implements Comparator<Ship> {
+
+    @Override
+    public int compare(Ship o1, Ship o2) {
+
+
+        double o1TraveledDistance = o1.getPositioningDataList().traveledDistance();
+        double o2TraveledDistance = o2.getPositioningDataList().traveledDistance();
+
+        double o1NumberMovements = o1.getPositioningDataList().getPositioningDataList().size();
+        double o2NumberMovements = o2.getPositioningDataList().getPositioningDataList().size();
+
+
+
+        if (o1TraveledDistance > o2TraveledDistance)
+            return 1;
+
+        else if (o1TraveledDistance < o2TraveledDistance)
+            return -1;
+
+
+        if (o1TraveledDistance == o2TraveledDistance){
+
+            if (o1NumberMovements == o2NumberMovements)
+                return 0;
+
+            else if (o1NumberMovements > o2NumberMovements)
+                return -1;
+
+            else return 1;
+        }
+
+        return 0;
+    }
+}
