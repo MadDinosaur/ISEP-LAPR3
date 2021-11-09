7323802d1a7cd2b3d21358b6577feb229ae11836
BarbaraDP <1191507@isep.ipp.pt>
2021-11-09 01:35:03 +0000

    LAP22G21-9 #comment generated file structure for data integrity tests.

---
+++ docs/DataIntegrityRestrictionsTest.sql
@@ -0,0 +1,22 @@
+-- DOMAIN RESTRICTIONS
+-- Tests if all the columns in the database are restricted to a particular domain.
+
+-- restriction
+
+-- test
+
+-- expected result
+
+
+
+-- IDENTITY RESTRICTIONS
+-- Tests if the primary key in every table is unique and not null.
+
+
+
+-- REFERENTIAL RESTRICTIONS
+-- Tests if the foreign key refers to a primary key value of some table in the database.
+
+
+-- APPLICATION RESTRICTIONS
+-- Tests other sets of rules defined by the business model.
