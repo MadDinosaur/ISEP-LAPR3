b76cf077a72f259c78b0948b102cafe7497abe55
Miguel Silva <1201045@isep.ipp.pt>
2021-11-11 17:00:22 +0000

    LAP22G21-5 #comment  Tests 100% done

--- src/test/java/lapr/project/store/ShipStoreTest.java
+++ src/test/java/lapr/project/store/ShipStoreTest.java
@@ -14,35 +14,50 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ShipStoreTest {
 
-    PositioningDataList positioningDataList = new PositioningDataList();
+    PositioningDataList positioningDataList1 = new PositioningDataList();
+    PositioningDataList positioningDataList2 = new PositioningDataList();
+
     ShipStore shipStore = new ShipStore();
-    PositioningData positioningData;
+    ShipSorter shipSorter = new ShipSorter();
+
     Coordinate coordinate;
-    PositioningData positioningData2;
     Coordinate coordinate2;
-    PositioningData positioningData3;
     Coordinate coordinate3;
+    Coordinate coordinate4;
+    Coordinate coordinate5;
+
+
+    PositioningData positioningData;
+    PositioningData positioningData2;
+    PositioningData positioningData3;
+    PositioningData positioningData4;
+    PositioningData positioningData5;
+    Ship s1;
+    Ship s2;
+
+
 
     @BeforeEach
     public void setUp(){
 
         coordinate = new Coordinate((float)-66.97001,((float)42.97875));
-        positioningData = new PositioningData("31/12/2020 17:20", coordinate, (float)12.9, (float)13.1, 355, "Sea", "B");
-        positioningDataList.insertPositioningDataList(positioningData);
         coordinate2 = new Coordinate((float)-66.97243, (float)42.92236);
-        positioningData2 = new PositioningData("31/12/2020 17:03", coordinate2, (float)12.5, (float)2.4, 358, "Sea","B");
-        positioningDataList.insertPositioningDataList(positioningData2);
         coordinate3 = new Coordinate((float) -66.9759, (float)42.7698);
+        coordinate4 = new Coordinate((float)42.97875, (float) -66.97001);
+        coordinate5 = new Coordinate((float)43.22513, (float) -66.96725);
+
+        positioningData = new PositioningData("31/12/2020 17:20", coordinate, (float)12.9, (float)13.1, 355, "Sea", "B");
+        positioningData2 = new PositioningData("31/12/2020 17:03", coordinate2, (float)12.5, (float)2.4, 358, "Sea","B");
         positioningData3 = new PositioningData("31/12/2020 16:20", coordinate3, (float)13.3, (float)3.7,356, "Sea","B");
-        positioningDataList.insertPositioningDataList(positioningData3);
+        positioningData4 = new PositioningData("31/12/2020 16:30", coordinate4, (float)15.3, (float)2.8,356, "Sea","B");
+        positioningData5 = new PositioningData("31/12/2020 16:32", coordinate5, (float)14.3, (float)2.7,356, "Sea","B");
 
+        positioningDataList1.insertPositioningDataList(positioningData);
+        positioningDataList1.insertPositioningDataList(positioningData2);
+        positioningDataList1.insertPositioningDataList(positioningData3);
 
-        PositioningDataList pList1 = new PositioningDataList();
-        PositioningDataList pList2 = new PositioningDataList();
-        ArrayList<PositioningData> pDataList1 = new ArrayList<>();
-        ArrayList<PositioningData> pDataList2 = new ArrayList<>();
-
-
+        positioningDataList2.insertPositioningDataList(positioningData4);
+        positioningDataList2.insertPositioningDataList(positioningData5);
 
         String shipName = "Example";
         String mmsi1 = "210950000";
@@ -59,38 +74,14 @@ class ShipStoreTest {
         float width = 0;
         float draft = 0;
 
-        Coordinate c1 = new Coordinate(42.97875f, (float) -66.97001);
-        Coordinate c2 = new Coordinate(43.22513f, (float) -66.96725);
+        s1 = new Ship(mmsi1,shipName,imo1,callSign1,vesselType,length,width,draft);
+        s2 = new Ship(mmsi2,shipName,imo2,callSign2,vesselType,length,width,draft);
 
-        Ship s1 = new Ship(mmsi1,shipName,imo1,callSign1,vesselType,length,width,draft);
-        Ship s2 = new Ship(mmsi2,shipName,imo2,callSign2,vesselType,length,width,draft);
-
-        String bdt = "31/12/2000 23:38";
-        float sog = 0;
-        float cog = 359;
-        float heading = 0;
-        String position = "95";
-        String transceiverClass = "A";
-
-        PositioningData d1 = new PositioningData(bdt,c1,sog,cog,heading,position,transceiverClass);
-        PositioningData d2 = new PositioningData(bdt,c2,sog,cog,heading,position,transceiverClass);
-
-        pDataList1.add(d1);
-        pDataList2.add(d2);
-
-        for (PositioningData positioningData : pDataList1)
-            pList1.insert(positioningData);
-        for (PositioningData positioningData : pDataList2)
-            pList2.insert(positioningData);
-
-        s1.setPositioningDataList(positioningDataList);
-        s2.setPositioningDataList(pList2);
-
-
+        s1.setPositioningDataList(positioningDataList1);
+        s2.setPositioningDataList(positioningDataList2);
 
         shipStore.addShip(s1);
         shipStore.addShip(s2);
-
     }
 
 
@@ -111,16 +102,28 @@ class ShipStoreTest {
 
     @Test
     public void shipsSortedTraveledDistanceToStringTest() {
-        ShipSorter shipSorter = new ShipSorter();
-
 
         TreeSet<Ship> treeShips = shipStore.sortShips(shipSorter);
         TreeSet<String> result = shipStore.shipsSortedTraveledDistanceToString(treeShips);
         TreeSet<String> expected = new TreeSet<>();
         expected.add("MMSI: 210950000 - Traveled Distance: 23,238516 - Number of Movements: 3");
-        expected.add("MMSI: 229857000 - Traveled Distance: 0,000000 - Number of Movements: 1");
+        expected.add("MMSI: 229857000 - Traveled Distance: 10,722226 - Number of Movements: 2");
 
         assertEquals(expected.toString().replaceAll(",","."),result.toString().replaceAll(",","."));
+    }
+
+    @Test
+    public void shipsSortedTraveledDistanceToStringSameTraveledDistanceTest(){
+        s2.setPositioningDataList(positioningDataList1);
+
+        TreeSet<Ship> treeShips = shipStore.sortShips(shipSorter);
+        TreeSet<String> result = shipStore.shipsSortedTraveledDistanceToString(treeShips);
+        TreeSet<String> expected = new TreeSet<>();
+        expected.add("MMSI: 210950000 - Traveled Distance: 23,238516 - Number of Movements: 3");
+        expected.add("MMSI: 229857000 - Traveled Distance: 23,238516 - Number of Movements: 3");
+
+        assertEquals(expected.toString().replaceAll(",","."),result.toString().replaceAll(",","."));
+
     }
 
 }
