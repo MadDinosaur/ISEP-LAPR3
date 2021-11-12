5e26f134fbf84981cfa48bacbef3b42cc7746518
BarbaraDP <1191507@isep.ipp.pt>
2021-11-12 00:00:02 +0000

    LAP22G21-7 #comment implemented ship comparator for ordering.

---
+++ src/main/java/lapr/project/utils/SorterTraveledDistByDiff.java
@@ -0,0 +1,34 @@
+package lapr.project.utils;
+
+import lapr.project.model.Ship;
+
+import java.util.Comparator;
+
+/**
+ * Sorts ships in descending order of the
+ * Travelled Distance difference from the @baseTraveledDistance
+ */
+public class SorterTraveledDistByDiff implements Comparator<Ship> {
+
+    private double baseTraveledDistance;
+
+    public SorterTraveledDistByDiff(double baseTraveledDistance) {
+        this.baseTraveledDistance = baseTraveledDistance;
+    }
+
+    /**
+     * Compares two ships based on the Travelled Distance difference from the @baseTraveledDistance
+     * @param o1
+     * @param o2
+     * @return 1 --> o1 traveled distance difference is lesser than o2
+     *        -1 --> o1 traveled distance difference is greater than o2
+     *         0 --> o1 traveled distance difference is equal to o2
+     */
+    @Override
+    public int compare(Ship o1, Ship o2) {
+        double o1TraveledDistDiff = Math.abs(baseTraveledDistance - o1.getPositioningDataList().traveledDistance());
+        double o2TraveledDistDiff = Math.abs(baseTraveledDistance - o2.getPositioningDataList().traveledDistance());
+
+        return -Double.compare(o1TraveledDistDiff, o2TraveledDistDiff);
+    }
+}
---
+++ src/test/java/lapr/project/utils/SorterTraveledDistByDiffTest.java
@@ -0,0 +1,72 @@
+package lapr.project.utils;
+
+import lapr.project.model.Coordinate;
+import lapr.project.model.PositioningData;
+import lapr.project.model.Ship;
+import lapr.project.store.list.PositioningDataList;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SorterTraveledDistByDiffTest {
+    SorterTraveledDistByDiff testSorter;
+    Ship s1, s2, s3;
+
+    @Test
+    void compareLesser() {
+        int expected  = 1;
+        int result = testSorter.compare(s1, s2);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void compareGreater() {
+        int expected  = -1;
+        int result = testSorter.compare(s2, s1);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void compareEqual() {
+        int expected  = 0;
+        int result = testSorter.compare(s1, s1);
+
+        assertEquals(expected, result);
+
+        result = testSorter.compare(s1,s3);
+
+        assertEquals(expected, result);
+    }
+
+    @BeforeEach
+    void setUp() {
+        s1 = new Ship("111111111", "TestShip1", 1111111, "TestCallsign1", 1, 0f, 0f, 0f);
+        s2 = new Ship("222222222", "TestShip2", 2222222, "TestCallsign2", 1, 0f, 0f, 0f);
+        s3 = new Ship("333333333", "TestShip3", 3333333, "TestCallsign3", 1, 0f, 0f, 0f);
+
+        //Distance Ship1 = 1111km - 1000km = 111km
+        PositioningData s1p1 = new PositioningData("01/01/2021 00:00", new Coordinate(0,0),0f,0f,0f,"TestPos","A");
+        PositioningData s1p2 = new PositioningData("01/12/2021 00:00", new Coordinate(0,10),0f,0f,0f,"TestPos","A");
+        PositioningDataList s1Pos = new PositioningDataList();
+        s1Pos.insertPositioningDataList(s1p1);
+        s1Pos.insertPositioningDataList(s1p2);
+        s1.setPositioningDataList(s1Pos);
+
+        //Distance Ship2 = 556km - 1000km = 444km
+        PositioningData s2p1 = new PositioningData("01/01/2021 00:00", new Coordinate(0,0),0f,0f,0f,"TestPos","A");
+        PositioningData s2p2 = new PositioningData("01/12/2021 00:00", new Coordinate(0,5),0f,0f,0f,"TestPos","A");
+        PositioningDataList s2Pos = new PositioningDataList();
+        s2Pos.insertPositioningDataList(s2p1);
+        s2Pos.insertPositioningDataList(s2p2);
+        s2.setPositioningDataList(s2Pos);
+
+        //DistanceShip3 = 1111km - 1000km = 111km
+        s3.setPositioningDataList(s1Pos);
+
+
+        testSorter = new SorterTraveledDistByDiff(1000.0);
+    }
+}
