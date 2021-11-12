c0abc089b6cc583ceb9d88500722aeca2f8c2462
BarbaraDP <1191507@isep.ipp.pt>
2021-11-12 21:15:58 +0000

    LAP22G21-7 #comment added controller class.

---
+++ src/main/java/lapr/project/controller/CloseShipRoutesController.java
@@ -0,0 +1,49 @@
+package lapr.project.controller;
+
+import lapr.project.data.MainStorage;
+import lapr.project.model.Ship;
+import lapr.project.store.ShipStore;
+import lapr.project.utils.SorterTraveledDistance;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.TreeSet;
+
+public class CloseShipRoutesController {
+
+    /**
+     *  The current ship store
+     */
+    private final ShipStore shipStore;
+
+    /**
+     * Calls the creator with the current storage instance
+     */
+    public CloseShipRoutesController() {
+        this(MainStorage.getInstance());
+    }
+
+    /**
+     * Creates a instance of the controller with the current storage instance
+     * @param storage the storage instance used to store all information
+     */
+    public CloseShipRoutesController(MainStorage storage) {
+        this.shipStore = storage.getShipStore();
+    }
+
+    /**
+     * Gets pairs of ships with close coordinates, ordered by 1st first MMSI (ascending) and Traveled Distance (descending)
+     * @return returns the sorted pairs of ships
+     */
+    public String getCloseShipRoutes() {
+
+        int maxMovements = 1000;
+        int maxTraveledDistance = 10;
+        int distance = 5;
+
+        HashMap<Ship, TreeSet<Ship>> routes = shipStore.getCloseShipRoutes(maxTraveledDistance,maxMovements, distance);
+
+        return shipStore.shipsPairedCloseRoutesToString(routes);
+    }
+
+}
