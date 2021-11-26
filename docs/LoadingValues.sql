a9ec4ac6ae8f4611153b1bb1c4eddea3d196c669
Carlos1200991 <1200991@isep.ipp.pt>
2021-11-26 19:36:36 +0000

    LAP22G21-11 #comment started work on loading file

---
+++ docs/LoadingValues.sql
@@ -0,0 +1,45 @@
+-- Inserts the values that have no relation to each other --
+
+-- Fleet --
+INSERT INTO Fleet(id)
+VALUES(1);
+INSERT INTO Fleet(id)
+VALUES(2);
+INSERT INTO Fleet(id)
+VALUES(3);
+
+-- VesselType --
+INSERT INTO VesselType(id)
+VALUES(1);
+INSERT INTO VesselType(id)
+VALUES(2);
+
+-- Captain --
+INSERT INTO Captain(id)
+VALUES(1);
+INSERT INTO Captain(id)
+VALUES(2);
+INSERT INTO Captain(id)
+VALUES(3);
+INSERT INTO Captain(id)
+VALUES(4);
+INSERT INTO Captain(id)
+VALUES(5);
+
+-- StorageType --
+INSERT INTO StorageType(name)
+VALUES('Port');
+INSERT INTO StorageType(name)
+VALUES('WareHouse');
+
+-- Certificate --
+INSERT INTO Certificate(id, name)
+VALUES(1, 'Certificate1');
+INSERT INTO Certificate(id, name)
+VALUES(2, 'Certificate2');
+INSERT INTO Certificate(id, name)
+VALUES(3, 'Certificate3');
+
+--  CscPlate --
+INSERT INTO CscPlate()
+VALUES()
