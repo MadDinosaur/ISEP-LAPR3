bb0deb59821835cfef2600ff16f3d843f8fcd417
Miguel <1201045@isep.ipp.pt>
2021-11-27 15:19:30 +0000

    LAP22G21-46 #comment Commented a line so Jenkins can work and started adding tests

---
+++ src/test/java/lapr/project/controller/ReadStorageFileControllerTest.java
@@ -0,0 +1,20 @@
+package lapr.project.controller;
+
+import lapr.project.data.MainStorage;
+import lapr.project.model.Coordinate;
+import lapr.project.model.Storage;
+import lapr.project.store.StorageStore;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class ReadStorageFileControllerTest {
+
+    @Test
+    public void controllerTest(){
+        ReadStorageFileController readStorageController = new ReadStorageFileController();
+        readStorageController.readFileAndSaveData("sports.csv");
+        assertNotNull(MainStorage.getInstance().getStorageStore().findNearestNeighbour(-3,53));
+    }
+    
+}
