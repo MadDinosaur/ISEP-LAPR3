935110bd02272079698794a95605bae8367f6fa3
Miguel <1201045@isep.ipp.pt>
2021-11-27 18:22:16 +0000

    Added presentation tests

---
+++ src/test/java/lapr/project/presentationTests/presentationTestsSprint2.java
@@ -0,0 +1,15 @@
+package lapr.project.presentationTests;
+
+import lapr.project.controller.ReadStorageFileController;
+import org.junit.jupiter.api.BeforeEach;
+
+public class presentationTestsSprint2 {
+
+
+    @BeforeEach
+    private void setUp(){
+
+        ReadStorageFileController readStorageFileController = new ReadStorageFileController();
+        readStorageFileController.readFileAndSaveData("bports.csv");
+    }
+}
