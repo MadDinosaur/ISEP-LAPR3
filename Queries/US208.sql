c3c9b7e67d5d5e5cd66ade5b030efa04d2aac8d5
MartimMaciel_1201154 <1201154@isep.ipp.pt>
2021-11-30 20:32:25 +0000

    LAP22G21-53 #comment added occupancy_rate function script

---
+++ Queries/US208.sql
@@ -0,0 +1,18 @@
+CREATE OR REPLACE FUNCTION occupancy_rate(id_ship ship.mmsi%type, manifest_id cargomanifest.id%type)
+RETURN number
+IS 
+    Ship_Capacity number(5,2);
+    Container_Volume number(4,1);
+
+BEGIN
+    SELECT (s.capacity), sum(con.max_volume)
+    INTO Ship_Capacity, Container_Volume
+    FROM cargomanifest c, container_cargoManifest cc, ship s, container con
+    WHERE s.mmsi = id_ship
+    AND c.id = manifest_id
+    AND c.ship_mmsi = s.mmsi
+    AND cc.cargo_manifest_id = manifest_id
+    AND cc.container_num = con.num
+    GROUP BY s.capacity, con.max_volume;
+    RETURN Container_Volume/Ship_Capacity;
+END;
