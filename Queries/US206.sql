e6a8d63b4582fab7c3595f5924e9879b0987738b
BarbaraDP <1191507@isep.ipp.pt>
2021-12-05 09:12:17 +0000

    LAP22G21-51 #comment updated query into procedure

--- Queries/US206.sql
+++ Queries/US206.sql
@@ -1,12 +1,34 @@
-SELECT NUM,
-       (CASE WHEN REFRIGERATED_FLAG = 1 THEN 'Refrigerated' ELSE 'Non-refrigerated' END) as TYPE,
-       CONTAINER_POSITION_X, CONTAINER_POSITION_Y, CONTAINER_POSITION_Z,
-       PAYLOAD
-FROM CONTAINER INNER JOIN CONTAINER_CARGOMANIFEST ON CONTAINER.NUM = CONTAINER_CARGOMANIFEST.CONTAINER_NUM
-WHERE CARGO_MANIFEST_ID IN
-      (SELECT ID
-      FROM CARGOMANIFEST
-      WHERE SHIP_MMSI = 100000001
-        AND CARGOMANIFEST.STORAGE_IDENTIFICATION = 2
-        AND LOADING_FLAG = 1
-        AND FINISHING_DATE_TIME IS NULL);
+CREATE OR REPLACE PROCEDURE containers_loaded(pShipMmsi cargomanifest.ship_mmsi%type, pPort cargomanifest.storage_identification%type)
+AS
+    vContainerNum container.num%type;
+    vType varchar(20);
+    vPositionX container_cargomanifest.container_position_x%type;
+    vPositionY container_cargomanifest.container_position_y%type;
+    vPositionZ container_cargomanifest.container_position_z%type;
+    vLoad container.payload%type;
+    CURSOR containers IS
+        SELECT NUM,
+        (CASE WHEN REFRIGERATED_FLAG = 1 THEN 'Refrigerated' ELSE 'Non-refrigerated' END) as "TYPE",
+        CONTAINER_POSITION_X, CONTAINER_POSITION_Y, CONTAINER_POSITION_Z,
+        PAYLOAD
+        FROM CONTAINER INNER JOIN CONTAINER_CARGOMANIFEST ON CONTAINER.NUM = CONTAINER_CARGOMANIFEST.CONTAINER_NUM
+        WHERE CARGO_MANIFEST_ID IN
+          (SELECT ID
+          FROM CARGOMANIFEST
+          WHERE SHIP_MMSI = pShipMmsi
+            AND CARGOMANIFEST.STORAGE_IDENTIFICATION = pPort
+            AND LOADING_FLAG = 1
+            AND FINISHING_DATE_TIME IS NULL);
+BEGIN
+    OPEN containers;
+    DBMS_OUTPUT.PUT_LINE('Container no. | Type | Position | Load');
+    LOOP
+        FETCH containers INTO vContainerNum, vType, vPositionX, vPositionY, vPositionZ, vLoad;
+        EXIT WHEN containers%NOTFOUND;
+        DBMS_OUTPUT.PUT_LINE(vContainerNum || ' | ' || vType || ' | (' || vPositionX || ',' || vPositionY || ',' || vPositionZ || ') | ' || vLoad);
+    end loop;
+    CLOSE containers;
+end;
+/
+
+call containers_loaded(100000001, 2);
