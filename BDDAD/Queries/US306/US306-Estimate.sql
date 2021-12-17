b75d548b834422b4385ac3726b35c314309772eb
MartimMaciel_1201154 <1201154@isep.ipp.pt>
2021-12-17 22:12:04 +0000

    LAP22G21-103 #time 1h15m #comment function to obtain estimate number of leaving containers

---
+++ BDDAD/Queries/US306/US306-Estimate.sql
@@ -0,0 +1,16 @@
+CREATE OR REPLACE FUNCTION func_estimate_number_leaving_containers(id_storage storage.identification%type)
+RETURN INTEGER
+IS
+    Leaving_Containers integer(7);
+    
+BEGIN
+    select count(container_num)
+    INTO Leaving_Containers
+    FROM STORAGE s, CARGOMANIFEST_PARTIAL cp, container_cargomanifest cc
+    where s.identification = id_storage
+    and s.identification = cp.storage_identification
+    and cp.id = cc.partial_cargo_manifest_id
+    and status like 'pending' 
+    and cp.finishing_date_time between current_timestamp and current_timestamp + 30;
+    RETURN Leaving_Containers;
+END;
