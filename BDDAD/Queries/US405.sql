c791f6ed997892ca508844d2a0c8e683274d9af3
MartimMaciel_1201154 <1201154@isep.ipp.pt>
2022-01-14 16:06:14 +0000

    LAP22G21-203 #time 4h #comment sql script for us405, missing expections

---
+++ BDDAD/Queries/US405.sql
@@ -0,0 +1,44 @@
+create or replace function func_average_occupancy_rate_period(id_ship ship.mmsi%type, start_date date, end_date date)
+return number
+is
+    Number_Manifests number;
+    vIdFull cargomanifest_full.id%type;
+    totalOccupancyRate number;
+    occupancyRate number;
+    
+cursor src is (select cf.id 
+                from cargomanifest_full cf
+                where cf.ship_mmsi = id_ship
+                and cf.finishing_date_time between start_date and end_date);
+                
+begin
+    -- gets number of manifests with same ship mmsi 
+    select count(cf.id)
+    into Number_Manifests
+    from cargomanifest_full cf
+    where cf.ship_mmsi = id_ship
+    and cf.finishing_date_time between start_date and end_date;
+    
+    totalOccupancyRate := 0;
+    
+    open src;
+    
+    Loop
+        Fetch src into vIdFull;
+        Exit When src%NOTFOUND;
+        
+        -- get occupancy rate of a manifest
+        select func_occupancy_rate(id_ship, vIdFull)
+        into occupancyRate
+        from dual;
+        
+        -- totalOccupancyRate += occupancyRate
+        
+        totalOccupancyRate := totalOccupancyRate + occupancyRate;
+        
+    End Loop;
+    
+    Close src;
+    
+    RETURN totalOccupancyRate/number_manifests;
+end;
